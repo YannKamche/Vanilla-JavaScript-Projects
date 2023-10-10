524360432908e50e8ee3ffbca39a35aae7a3b736
Yann Kamche <yannkamche@gmail.com>
2023-10-10 15:44:20 +0100

    Completed the questionnaire project

---
+++ 07-questions/setup/app.js
@@ -0,0 +1,34 @@
+//using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(question => {
+    
+    // select the button from within a question
+    const btn = question.querySelector('.question-btn');
+
+    btn.addEventListener('click', () => {
+        questions.forEach(item => {
+            if (item !== question) {
+                item.classList.remove('show-text');
+            }
+        });
+            question.classList.toggle('show-text');
+    })
+})
+
+
+
+
+
+
+// traversing the dom
+
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', e => {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     })
+// })
